122731ba1b98a1b219ec647e0bef907811608d4b
Jose Kaio <kaios5028@gmail.com>
2025-02-12 21:05:20 -0300

    https://controle-estoque-api-production.up.railway.app/

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://192.168.0.8:3001'],
+    origin: ['http://localhost:3001', 'http://192.168.0.8:3001', 'https://controle-estoque-api-production.up.railway.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
